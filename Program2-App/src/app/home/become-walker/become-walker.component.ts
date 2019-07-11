c60de61679c9b27207cdf76572beb6a726fe29ae
mikaelaenters <mikaelaenters@icloud.com>
2019-07-11 18:37:54 -0400

    THIS IS THE ACTUAL PROJECTgit add *!

---
+++ Program2-App/src/app/home/become-walker/become-walker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-become-walker',
+  templateUrl: './become-walker.component.html',
+  styleUrls: ['./become-walker.component.css']
+})
+export class BecomeWalkerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
