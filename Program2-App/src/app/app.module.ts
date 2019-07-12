8ee1feed227026321975ffbe361dbfd024fd671a
mikaelaenters <mikaelaenters@icloud.com>
2019-07-11 20:25:26 -0400

    Final routing changes

--- Program2-App/src/app/app.module.ts
+++ Program2-App/src/app/app.module.ts
@@ -6,13 +6,12 @@ import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import { HomeModule } from './home/home.module';
 import { RouterModule } from '@angular/router';
-import { routes } from './app.router';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { CustomerFormComponent } from './customer-form/customer-form.component';
-import { HomeContainerComponent } from './home-container/home-container.component';
 import { HomeComponent } from './home/home.component';
+import { routes } from './app.router';
 
 
 
@@ -20,13 +19,11 @@ import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     CustomerFormComponent,
-    HomeContainerComponent,
     HomeComponent
   ],
   imports: [
-    RouterModule.forRoot([routes]),
+    RouterModule.forRoot(routes),
     HomeModule,
     MatButtonModule,
     MatCardModule,
--- Program2-App/src/app/app.router.ts
+++ Program2-App/src/app/app.router.ts
@@ -1,4 +1,4 @@
 import { Routes } from '@angular/router';
-import { HomeRoutes } from './home/home.router'; 
+import { HomeRoutes } from './home/home.router';
 
-export const routes: Routes [...HomeRoutes];
+export const routes: Routes = [...HomeRoutes];
